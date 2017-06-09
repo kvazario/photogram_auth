0edb80b35aef6af5d99a567ed1744b942a264439
Manuk <qvazar@gmail.com>
2017-06-09 11:25:18 -0500

    FInal

---
+++ app/models/photo.rb
@@ -0,0 +1,14 @@
+class Photo < ApplicationRecord
+
+  has_many :fans, :through => :likes, :source => :user
+
+  belongs_to :user, :class_name => "User", :foreign_key => "user_id"
+
+  has_many :comments, :class_name => "Comment", :foreign_key => "photo_id"
+
+  has_many :likes, :class_name => "Like", :foreign_key => "photo_id"
+
+  validates :user_id, :presence => true
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+
+  has_many :liked_photos, :through => :likes, :source => :photo
+
+  has_many :photos, :class_name => "Photo", :foreign_key => "user_id"
+
+  has_many :comments, :class_name => "Comment", :foreign_key => "user_id"
+
+  has_many :likes, :class_name => "Like", :foreign_key => "user_id"
+
+  validates :username, :presence => true, :uniqueness => true
+
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
